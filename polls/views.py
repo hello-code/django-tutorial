394bb255a288b23280dfd0d20468ce560933b775
z <z@m>
2015-01-27 11:15:43 +0800

    add results.html

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.template import RequestContext,loader
-from polls.models import Question
+from polls.models import Question,Choice
 from django.shortcuts import render,get_object_or_404
 from django.http import Http404
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
 def index(request):
     # qlist=Question.objects.all()
@@ -29,7 +31,19 @@ def detail(request,question_id):
     return render(request,'polls/detail.html',{'question':q})
 
 def results(request,question_id):
-    return HttpResponse('results is %s,'%question_id)
+    question=get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
 
 def vote(request,question_id):
-    return HttpResponse('voting on question %s.'%question_id)
+    p=get_object_or_404(Question,pk=question_id)
+    try:
+        selected=p.choice_set.get(pk=request.POST['choice'])
+    except(KeyError,Choice.DoesNotExist):
+        return render(request,'polls/detail.html',{
+            'question':p,
+            'error_message':'pleasssse select'
+        })
+    else:
+        selected.votes+=1
+        selected.save()
+    return HttpResponseRedirect(reverse('polls:results',args=(p.id,)))
